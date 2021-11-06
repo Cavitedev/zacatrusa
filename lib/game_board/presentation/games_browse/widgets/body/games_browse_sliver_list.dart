07e71cd0f2a46f5581e4880ee3c4cc79e03d0ca1
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-11-06 12:51:39 +0100

    Fix list view #19

--- lib/game_board/presentation/games_browse/widgets/body/games_browse_sliver_list.dart
+++ lib/game_board/presentation/games_browse/widgets/body/games_browse_sliver_list.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:zacatrusa/constants/app_margins.dart';
 
 import '../../../../zacatrus/domain/game_overview.dart';
 
@@ -35,33 +38,46 @@ class ListGameItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (game.image != null)
+          if (game.image != null && game.image!.imageLink != null)
             ExtendedImage.network(
               game.image!.imageLink!,
-              cache: true,
+              width: min(200, MediaQuery.of(context).size.width / 2.2),
+              semanticLabel: game.image?.imageAlt,
             ),
           Flexible(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 80.0),
+                  padding: const EdgeInsets.fromLTRB(5, 10, 0, 10),
                   child: Text(
                     game.name,
                     style: Theme.of(context).textTheme.headline5,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
-                if (game.numberOfComments != null)
-                  if (game.numberOfComments! > 1)
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 0, 0, 10.0),
-                      child: Text(
-                          game.numberOfComments!.toString() + " comentarios"),
-                    ),
-                if (game.numberOfComments == 1)
+                if (game.numberOfComments != null && game.numberOfComments! > 1)
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(5, 0, 0, 10.0),
+                    padding: const EdgeInsets.fromLTRB(
+                        5, 0, 0, innerElementsPadding),
+                    child: Text(
+                      game.numberOfComments!.toString() +
+                          (game.numberOfComments! > 1
+                              ? " comentarios"
+                              : " comentario"),
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ),
+                if (game.numberOfComments != null && game.numberOfComments == 1)
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(
+                        5, 0, 0, innerElementsPadding),
                     child:
                         Text(game.numberOfComments!.toString() + " comentario"),
                   ),
@@ -74,10 +90,17 @@ class ListGameItem extends StatelessWidget {
                     rating: game.stars!,
                     itemSize: 25.0,
                   ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5, 10.0, 0, 0),
-                  child: Text(
-                    game.price.toString() + " €",
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(
+                        5, innerElementsPadding, 0, innerElementsPadding),
+                    child: Text(
+                      game.price!.toStringAsFixed(2) + " €",
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle2!
+                          .copyWith(fontWeight: FontWeight.w600),
+                    ),
                   ),
                 ),
               ],
