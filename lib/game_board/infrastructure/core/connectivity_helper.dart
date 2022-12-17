2e2521b8b611d1da1b17c444fc4f1fc955fc46de
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2022-12-17 16:45:16 +0100

    retry reconnection update

--- lib/game_board/infrastructure/core/connectivity_helper.dart
+++ lib/game_board/infrastructure/core/connectivity_helper.dart
@@ -1,11 +1,12 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import '../http_loader.dart';
 
-extension ConnectivityX on Connectivity{
-
-
-    Future<ConnectivityResult> onConnectionFound() async {
-    return onConnectivityChanged.firstWhere((
-        connectivityResult) => connectivityResult != ConnectivityResult.none);
+extension ConnectivityX on Connectivity {
+  Future<ConnectivityResult> onConnectionFound() async {
+    return onConnectivityChanged.firstWhere(
+            (connectivityResult) {
+              print(connectivityResult);
+              return internetConnections.contains(connectivityResult);
+  });
   }
-
 }
